d5a96b8213eb86092754ea495b0256a3ef854b47
Crys7a1 <2577746048@qq.com>
2022-08-05 14:40:29 +0800

    loop arr

---
+++ 20220805/loopArr.js
@@ -0,0 +1,13 @@
+//1.数组的遍历
+{
+  let arr = [1, 2, 3, 5, 7, 8, 4, 6];
+  arr.forEach((v, i) => {
+    console.log(v, i);
+  });
+}
+{
+  let arr = [1, 2, 3, 5, 7, 8, 4, 6];
+  for (const item of arr) {
+    console.log(item);
+  }
+}
