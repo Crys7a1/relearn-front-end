0fc1793c47e9727c205367dd96e99402d577059f
Crys7a1 <2577746048@qq.com>
2022-08-05 17:59:22 +0800

    homework finished

---
+++ 20220805/homework/defineObj.js
@@ -0,0 +1,19 @@
+"use strict";
+const person = {
+  name: "->->",
+  gender: "male",
+  address: {
+    province: "福建省",
+    city: "福州市",
+  },
+  introduce() {
+    console.log("hello");
+  },
+  hair() {
+    console.log("hair");
+  },
+  beat() {
+    console.log("beat");
+  },
+};
+console.log(person);
