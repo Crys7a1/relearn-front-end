59a24102fccabd7e4ff82da673a9c97e537452f5
Crys7a1 <2577746048@qq.com>
2022-08-08 09:00:15 +0800

    config format

--- 20220805/Array_api.js
+++ 20220805/Array_api.js
@@ -1,13 +1,13 @@
 {
   console.log(`map方法 对数组中的每个元素操作 *2`);
   let arr = [-1, -3, 1, -5, 10, 7, 9];
-  let arr2 = arr.map(value => value * 2);
+  let arr2 = arr.map((value) => value * 2);
   console.log(arr2);
 }
 {
   console.log(`filter方法 过滤数组中小于0的元素`);
   let arr = [-1, -3, 1, -5, 10, 7, 9];
-  arr = arr.filter(value => value >= 0);
+  arr = arr.filter((value) => value >= 0);
   console.log(arr);
 }
 {
@@ -22,12 +22,12 @@
 {
   console.log(`every 方法 判断每个值是否大于0`);
   let arr = [1, 2, 3, 4, 5, 6];
-  let flag = arr.every(value => value > 0);
+  let flag = arr.every((value) => value > 0);
   console.log(flag);
 }
 {
   console.log(`some 方法 判断至少有一个值大于0`);
   let arr = [-1, -2, -3, -4, -5, -6];
-  let flag = arr.some(value => value > 0);
+  let flag = arr.some((value) => value > 0);
   console.log(flag);
 }
--- 20220805/对象的属性/obj.js
+++ 20220805/对象的属性/obj.js
@@ -1,35 +1,35 @@
-"use strict";
+'use strict'
 /*
 对象的计算属性[]
 和.属性的区别
 使用obj[xx]和obj.xxx
 */
 {
-  let arr = [];
+  let arr = []
   //将数组当做对象来用
-  arr["福建"] = "福州";
+  arr['福建'] = '福州'
   for (const key in arr) {
-    console.log(key);
+    console.log(key)
   }
-  console.log(arr);
+  console.log(arr)
 }
 {
-  let cs = new Object();
-  cs.name = "crystal";
-  cs.gender = "male";
-  cs._age = 21;
-  cs["address"] = "福建省";
-  cs["city postcode"] = "361000";
-  cs[0] = "c";
-  console.log(cs);
+  let cs = new Object()
+  cs.name = 'crystal'
+  cs.gender = 'male'
+  cs._age = 21
+  cs['address'] = '福建省'
+  cs['city postcode'] = '361000'
+  cs[0] = 'c'
+  console.log(cs)
   for (const key in cs) {
-    console.log(key);
+    console.log(key)
   }
   //判断一个对象中是否有某个属性
-  console.log("age" in cs);
-  console.log("_age" in cs);
-  console.log(cs.hasOwnProperty("address"));
+  console.log('age' in cs)
+  console.log('_age' in cs)
+  console.log(cs.hasOwnProperty('address'))
   //删除属性
-  delete cs["city postcode"];
-  console.log(cs);
+  delete cs['city postcode']
+  console.log(cs)
 }
