165b60edbab58a89109a3dbab28ed6788356044c
Crys7a1 <2577746048@qq.com>
2022-08-08 18:08:21 +0800

    unfinished

---
+++ 20220808/1.js
@@ -0,0 +1,18 @@
+// debugger
+// const element = document.createElement('p')
+// element.innerHTML = "<script>console.log('hello')</script>"
+// console.log(element.innerHTML)
+
+// 克隆节点
+// const p1 = document.querySelector('#p1')
+// const newNode = p1.cloneNode(true)
+// newNode.setAttribute('id', 'p3')
+// console.log(newNode)
+// document.body.appendChild(newNode)
+
+// const list = document.querySelector('#list')
+
+//只能在ie中使用
+// var body = document.body
+// body.innerHTML = "<script defer>alert('hello')</script>"
+
